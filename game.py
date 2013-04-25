169938ff54c6827de2780e4ab04e44ecf623bdc7
Oleg Grishin <og402@nyu.edu>
2013-04-25 23:29:37 +0400

    Ability to destroy rocks added. Moving platforms implemented. Menu improved. Graphics enhanced. Character animation redrawn. Numerous debugs

--- game.py
+++ game.py
@@ -4,10 +4,11 @@ Final project, Introduction to Computer Science, Fall 2012, NYUAD
 Oleg Grishin
 """
 
-import pygame, os
-import time
+import pygame, os, time
 from random import *
 from pygame.locals import *
+
+developer = False
 
 # Floor class handles collide events for the character to stay on the floor. Not visible
 
@@ -30,6 +31,39 @@ class Platform(pygame.sprite.Sprite):
         self.tech_sprite_lower = Floor(self.rect.width - 34,self.rect.left + 17,self.rect.top - 1)
         self.tech_sprite_upper = Floor(self.rect.width - 34,self.rect.left + 17,self.rect.top - 46)
         #dimensions of the character sprite + some space for falspeed
+
+#moving platforms - elevators
+#inheritance is not aplicable due to using sprites 
+class Elevator(pygame.sprite.Sprite):
+    def __init__(self, left, top, bottom, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('res/platform.png')
+        self.rect = self.image.get_rect()
+        self.rect.left = left
+        self.rect.top = bottom
+        self.limittop = top
+        self.limitbot = bottom
+        self.tech_sprite_lower = Floor(self.rect.width - 34,self.rect.left + 17,self.rect.top - 1)
+        self.tech_sprite_upper = Floor(self.rect.width - 34,self.rect.left + 17,self.rect.top - 46)
+        self.speed = speed
+        self.up = True
+        #dimensions of the character sprite + some space for falspeed
+
+    def update(self):
+        if self.rect.top < self.limittop:
+            self.up = False
+        if self.rect.top > self.limitbot:
+            self.up = True
+        if self.up:
+            self.rect.top -= self.speed
+            self.tech_sprite_lower.rect.top -= self.speed
+            self.tech_sprite_upper.rect.top -= self.speed
+        else:
+            self.rect.top += self.speed
+            self.tech_sprite_lower.rect.top += self.speed
+            self.tech_sprite_upper.rect.top += self.speed
+        
+        
 
 class Coin(pygame.sprite.Sprite):
     def __init__(self,left,top):
@@ -99,10 +133,13 @@ class Rock (pygame.sprite.Sprite):
             self.shake()
         if self.anticipated  in range (self.anticipation * 2 /3, self.anticipation):
             self.red ()
-        elif self.anticipated > self.anticipation and self.rect.top < 300:
+        elif self.anticipated > self.anticipation:
             self.fall()
         else:
             self.vy = 0
+
+        if self.rect.top > 300:
+            self.kill()
             
         self.anticipated += 1
         self.rect.top += self.vy
@@ -118,9 +155,9 @@ class Aladdin (pygame.sprite.Sprite):
         self.stay = pygame.image.load('res/aladdin.png')
         self.frames = [pygame.image.load('res/aladdin.png'),pygame.image.load('res/aladdin1.png'),\
                        pygame.image.load('res/aladdin2.png'),pygame.image.load('res/aladdin3.png'),\
-                       pygame.image.load('res/aladdin4.png'),pygame.image.load('res/aladdin5.png'),\
-                       pygame.image.load('res/aladdin6.png'),pygame.image.load('res/aladdin7.png'),\
-                       pygame.image.load('res/aladdin8.png')]
+                       pygame.image.load('res/aladdin4.png'),]
+        self.jump_img = pygame.image.load('res/jump.png')
+        self.jump_f = pygame.image.load('res/jump_f.png')
         self.frame = 0
         self.image = self.stay
 
@@ -141,7 +178,8 @@ class Aladdin (pygame.sprite.Sprite):
         self.jumping = False
         self.jumpable = False
         self.start = time.time()
-        self.t= time.time() - self.start
+        self.paused = time.time() - time.time() #to make it 0 in time format
+        self.t= time.time()
         self.text = pygame.font.Font('res/font/04B_03__.ttf', 14)
         self.text1 = pygame.font.Font('res/font/04B_03__.ttf', 10)
         self.text2 = pygame.font.Font('res/font/04B_03__.ttf', 96)
@@ -149,8 +187,8 @@ class Aladdin (pygame.sprite.Sprite):
         self.message = self.text.render('', False, (255,255,255))
         self.message1 = self.text.render('', False, (255,255,255))
         self.level = level
-        self.message2 = self.text.render('level 0' + str(level), False, (255,255,255))
-        self.message3 = self.text2.render('level 0' + str(level), False, (255,255,255))
+        self.message2 = self.text.render('level 0' + str(self.level), False, (255,255,255))
+        self.message3 = self.text2.render('level 0' + str(self.level), False, (255,255,255))
         Aladdin.coins = 0
         self.rocks = []
         self.globalrocks = game.rocks
@@ -159,9 +197,17 @@ class Aladdin (pygame.sprite.Sprite):
         self.rock_anticipation = rock_anticipation
         self.opacity = 255
         self.opacitywait = 0
+        self.pause = time.time()
+        self.lag = 0
 
+    def pause_t(self, flag):
+        if not flag:
+            self.pause = time.time()
+        else:
+            self.pause = time.time() - self.pause
+            self.paused += self.pause
         
-
+        
 
     def jump(self, sound):
         if self.jumpable:
@@ -174,9 +220,8 @@ class Aladdin (pygame.sprite.Sprite):
         if self.opacitywait < 15:
             self.opacitywait += 1
         else:
-            if self.opacity > 0:
-                self.message3.set_alpha(self.opacity)
-                self.opacity -= 9
+            self.message3.set_alpha(self.opacity)
+            self.opacity -= 9
     
         if self.jumping:
             if self.jumptimer < self.jumpduration:
@@ -195,29 +240,33 @@ class Aladdin (pygame.sprite.Sprite):
                 newgame(self.level+1)
             else:
                 self.warning = self.text.render('collect all coins first!', False, (255,0,0))
-        
-        if self.vy != 0 and self.vx >= 0:
-            self.image = self.frames[3]
+
+        self.lag += 1
+        self.lag %= 2
+        if self.vy != 0 and self.vx > 0:
+            self.image = self.jump_img
         elif self.vy != 0 and self.vx < 0:
-            self.image = self.frames[3]
+            self.image = self.jump_img
             self.image = pygame.transform.flip(self.image, True, False)
+        elif self.vy !=0 and self.vx == 0:
+            self.image = self.jump_f
         elif self.vx == 0:
             self.image = self.stay
-        elif self.vx > 0:
-            self.image = self.frames[self.frame]
+        elif self.vx > 0 and self.lag == 0:
+            self.image = self.frames[self.frame + 1]
             self.frame += 1
-            self.frame %= len(self.frames)
-        elif self.vx < 0:
-            self.image = self.frames[self.frame]
+            self.frame %= len(self.frames) - 1
+        elif self.vx < 0 and self.lag == 0:
+            self.image = self.frames[self.frame + 1]
             self.image = pygame.transform.flip(self.image, True, False)
             self.frame += 1
-            self.frame %= len(self.frames)
+            self.frame %= len(self.frames) - 1
         
         self.y += self.vy
         self.rect.left = self.x
         self.rect.top = self.y
         self.message = self.text.render(str(Aladdin.coins )+ '/5', False, (255,255,255))
-        self.t= time.time() - self.start
+        self.t= time.time() - self.start - self.paused
         self.rockfrequency += 1
         self.rockfrequency %= self.rock_frequency #lover in order to increase frequency of rocks falling
         if self.rockfrequency == 0:
@@ -242,11 +291,11 @@ class Game():
         self.character = pygame.sprite.Group(self.aladdin)
         
         #technical sprites for platforms
-        """
-        for item in platforms:
-            self.sprites.add(item.tech_sprite_lower,item.tech_sprite_upper)
-        """
-    
+        if developer:
+            for item in platforms:
+                self.sprites.add(item.tech_sprite_lower,item.tech_sprite_upper)
+            
+        
         self.floorsprites = pygame.sprite.Group(self.floor)
         self.stopsprites = pygame.sprite.Group(self.stops)
 
@@ -275,34 +324,33 @@ def newgame(level):
                         Platform (200,200),Platform (186, 120), Platform (266, 90)],\
                     [Coin(116,45), Coin(56,132), Coin(282,72), Coin (216,182), Coin (385,55)],\
                      40,50)
+        
         #to put coins on platforms: (x+16,y-18)  
         times = []
         
     if level == 2:
         times.append(round(game.aladdin.t,1))
         game = Game (level, [Platform (1, 138), Platform (48, 180),Platform (48, 99),\
-                     Platform (115, 170), Platform (115, 83), Platform (190, 82),\
-                     Platform (235, 112), Platform (286, 142), Platform (235, 172),\
-                     Platform (284, 200), Platform (340, 118), Platform (400, 90),\
-                     Platform (460, 120)],\
-                    [Coin(64,142), Coin(131,116), Coin(131,67), Coin (310,50), Coin (476,102)],\
+                             Platform (115, 170), Platform (115, 83), Platform (190, 82),\
+                             Platform (340, 118), Platform (400, 90),\
+                             Platform (460, 120), Elevator(265, 70, 230, 3)],\
+                     [Coin(64,142), Coin(131,116), Coin(131,67), Coin (310,50), Coin (476,82)],\
                      30, 40)
 
     if level == 3:
         times.append(round(game.aladdin.t,1))
-        game = Game (level, [Platform (0, 140), Platform (55, 75), Platform (120, 100), \
+        game = Game (level, [Platform (0, 140), Platform (55, 75), Platform (120, 100),\
                              Platform (195, 75), Platform (275, 95), Platform (350, 75),\
-                             Platform (430, 100), Platform (485, 135), Platform (445, 175)],\
-                    [Coin(16,122), Coin(136,82), Coin(291,77), Coin (475,35), Coin (75,155)],\
+                             Elevator(450, 70, 200, 4)],\
+                     [Coin(16,122), Coin(136,82), Coin(291,77), Coin (475,35), Coin (75,155)],\
                      25, 35)
         
     if level == 4:
         times.append(round(game.aladdin.t,1))
-        game = Game (level, [Platform (190, 190), Platform (245, 150), Platform (190, 120), \
-                             Platform (245, 83)],\
-                    [Coin(225,35), Coin(115,25), Coin(140,150), Coin (330,20), Coin (345,130)],\
-                     20, 35)
-
+        game = Game (level, [Elevator(220, 70, 220, 5)],\
+                     [Coin(225,35), Coin(115,25), Coin(140,150), Coin (330,20), Coin (345,130)],\
+                     20,35)
+        
     if level == 5:
         times.append(round(game.aladdin.t,1))
         game = Game (level, [Platform (129, 208), Platform (49, 186), Platform (100, 134), \
@@ -335,7 +383,7 @@ def main():
     screen = pygame.display.set_mode((width, height),0,32) #trying out 32 colors
     pygame.display.set_caption("Aladdin's Escape")
     bg = pygame.image.load('res/bg.png').convert()
-    screen.blit(bg, (0,0))
+    bg1 = pygame.image.load('res/pause.png').convert_alpha()
     coin = pygame.image.load('res/coin.png')
     coin = pygame.transform.scale(coin,(14, 14))
     timer = pygame.image.load('res/time.png')
@@ -344,6 +392,7 @@ def main():
 
 #game
     running = True
+    pause = False
 
 #to collect total time
     times = []
@@ -397,6 +446,7 @@ def main():
     over_s = pygame.mixer.Sound('res/over.aiff')
     win_s = pygame.mixer.Sound('res/win.wav')
     rock_s = pygame.mixer.Sound('res/rock.wav')
+    rock_s.set_volume(0.5)
     tick_s.set_volume(0.5) #adjusting loud sound
     
     
@@ -427,6 +477,16 @@ def main():
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
                         game.aladdin.jump(jump_s)
+                    if event.key == pygame.K_p:
+                        game.aladdin.pause_t(pause)
+                        if not pause:
+                            volume /= 2
+                            game.aladdin.message3 = game.aladdin.text2.render('', False, (255,255,255))
+                        else:
+                            if game.aladdin.opacity > 0:
+                                game.aladdin.message3 = game.aladdin.text2.render('level 0' + str(game.aladdin.level), False, (255,255,255))
+                            volume *= 2
+                        pause = not pause
                     if event.key == pygame.K_ESCAPE:
                         frame = "menu"
                         pygame.mixer.music.load('res/menu_bg.mp3')
@@ -467,6 +527,17 @@ def main():
                     game.aladdin.y = item.tech_sprite_lower.rect.top - game.aladdin.rect.height + 1
                     game.aladdin.jumpable = True
 
+                    if item.__class__.__name__ == 'Elevator':
+                        game.aladdin.vy = 0
+                        game.aladdin.jumpable = True
+                        if item.up:
+                            game.aladdin.y = item.tech_sprite_lower.rect.top - game.aladdin.rect.height + 1 - item.speed
+                        else:
+                            game.aladdin.y = item.tech_sprite_lower.rect.top - game.aladdin.rect.height + 1 + item.speed
+                            
+                        
+                    
+
             for item in game.coins:
                 if pygame.sprite.collide_rect(game.aladdin,item):
                     item.collect(coin_s)
@@ -476,29 +547,35 @@ def main():
 
                 
             #character group is made to make sure that the character is drawn on top of everything
-            screen.blit(bg, (0,0))
-            game.sprites.clear(screen,bg)
-            game.character.clear(screen,bg)
-            game.rocks.clear(screen,bg)
-            game.sprites.update()
-            game.character.update()
-            game.rocks.update()
-            game.sprites.draw(screen)
-            game.character.draw(screen)
-            game.rocks.draw(screen)
-            screen.blit(game.aladdin.message3, (94,75))
-            screen.blit(game.aladdin.message,(483,257))
-            screen.blit(game.aladdin.message1,(483,275))
-            screen.blit(game.aladdin.message2,(460,242))
-            screen.blit(game.aladdin.warning, (360, 150))
-            screen.blit(coin, (463,256))
-            screen.blit(timer, (463,273))
+
+                screen.blit(bg, (0,0))
+                game.sprites.clear(screen,bg)
+                game.character.clear(screen,bg)
+                game.rocks.clear(screen,bg)
+                game.sprites.draw(screen)
+                game.character.draw(screen)
+                game.rocks.draw(screen)
+                screen.blit(game.aladdin.message3, (85,75))
+                screen.blit(game.aladdin.message,(483,257))
+                screen.blit(game.aladdin.message1,(483,275))
+                screen.blit(game.aladdin.message2,(460,242))
+                screen.blit(game.aladdin.warning, (360, 150))
+                screen.blit(coin, (463,256))
+                screen.blit(timer, (463,273))
+
+            #no game updates if the game is paused
+            if not pause:
+                game.sprites.update()
+                game.character.update()
+                game.rocks.update()
+            else:
+                screen.blit(bg1,(0,0))
 
             #draw technical sprites for debugging
-            """
-            game.floorsprites.draw(screen)
-            game.stopsprites.draw(screen)
-            """
+            if developer:
+                game.floorsprites.draw(screen)
+                game.stopsprites.draw(screen)
+            
 
         if frame == "menu":
             
@@ -774,7 +851,7 @@ def main():
                 jump_s.set_volume(volume/5.0)
                 over_s.set_volume(volume/5.0)
                 win_s.set_volume(volume/5.0)
-                rock_s.set_volume(volume/5.0)
+                rock_s.set_volume(volume/10.0)
 
         if musicon == 0:
             pygame.mixer.music.set_volume(0.0)
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Remember the story of Disney's Aladdin? When he gets the lamp, the cave starts to collapse. Aladdin then escapes it. The game represents this very moment of the story. Escape the cave!
+
+Go through 5 level to win. Collect all coing on a level to proceed. Use arrow keys to run and jump. You get killed if a falling rock hits you. Destroy a rock by hitting it before it falls. Press ESC to quit and P to pause. Fast! Your time is your score. 
+
+The game is created as a final project for Introduction to CS at NYU Abu Dhabi by Oleg Grishin. 
+Contact: og402@nyu.edu 
+
+In order to run the game, execute game.py (or use shortcut for exe version).
+
+I do not own the rights to the background image, sounds, the game font and Disney's Aladdin character.
